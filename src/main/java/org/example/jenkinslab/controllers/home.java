979e1b032504a604e0e3aa3ed29f8fcd72ef5673
mr3nz1 <paterne.murenzi@amalitech.org>
2024-09-20 09:43:14 +0200

    ch: setup kubernetes

--- src/main/java/org/example/jenkinslab/controllers/home.java
+++ src/main/java/org/example/jenkinslab/controllers/home.java
@@ -1,11 +1,13 @@
 package org.example.jenkinslab.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class home {
-    @GetMapping({"", "/", "/home"})
+    @GetMapping({"home"})
     public String home() {
         return "Hello world";
     }
